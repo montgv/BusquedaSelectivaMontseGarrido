ff166de7da4cda0e9d3548e2608e88421cbc68b0
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2022-11-24 23:17:33 +0100

    Agregar archivos de proyecto.

---
+++ BusquedaSelectivaMontseGarrido/FormPrincipal.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace BusquedaSelectivaMontseGarrido
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTitulo = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btIzq = new System.Windows.Forms.Button();
+            this.btDer = new System.Windows.Forms.Button();
+            this.lbBusqueda = new System.Windows.Forms.Label();
+            this.tbBuscar = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbTitulo
+            // 
+            this.lbTitulo.AutoSize = true;
+            this.lbTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitulo.Location = new System.Drawing.Point(341, 45);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.Size = new System.Drawing.Size(398, 37);
+            this.lbTitulo.TabIndex = 0;
+            this.lbTitulo.Text = "Búsqueda Selectiva DGV";
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(38, 167);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 62;
+            this.dgv.RowTemplate.Height = 28;
+            this.dgv.Size = new System.Drawing.Size(895, 337);
+            this.dgv.TabIndex = 1;
+            // 
+            // btIzq
+            // 
+            this.btIzq.BackColor = System.Drawing.SystemColors.Window;
+            this.btIzq.Location = new System.Drawing.Point(38, 105);
+            this.btIzq.Name = "btIzq";
+            this.btIzq.Size = new System.Drawing.Size(68, 28);
+            this.btIzq.TabIndex = 2;
+            this.btIzq.Text = "<";
+            this.btIzq.UseVisualStyleBackColor = false;
+            this.btIzq.Click += new System.EventHandler(this.btIzq_Click);
+            // 
+            // btDer
+            // 
+            this.btDer.BackColor = System.Drawing.SystemColors.Window;
+            this.btDer.Location = new System.Drawing.Point(853, 105);
+            this.btDer.Name = "btDer";
+            this.btDer.Size = new System.Drawing.Size(80, 28);
+            this.btDer.TabIndex = 3;
+            this.btDer.Text = ">";
+            this.btDer.UseVisualStyleBackColor = false;
+            this.btDer.Click += new System.EventHandler(this.btDer_Click);
+            // 
+            // lbBusqueda
+            // 
+            this.lbBusqueda.AutoSize = true;
+            this.lbBusqueda.Location = new System.Drawing.Point(202, 109);
+            this.lbBusqueda.Name = "lbBusqueda";
+            this.lbBusqueda.Size = new System.Drawing.Size(191, 20);
+            this.lbBusqueda.TabIndex = 5;
+            this.lbBusqueda.Text = "Realiza aquí la búsqueda:";
+            // 
+            // tbBuscar
+            // 
+            this.tbBuscar.Location = new System.Drawing.Point(400, 107);
+            this.tbBuscar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbBuscar.Name = "tbBuscar";
+            this.tbBuscar.Size = new System.Drawing.Size(362, 26);
+            this.tbBuscar.TabIndex = 6;
+            this.tbBuscar.TextChanged += new System.EventHandler(this.tbBuscar_TextChanged);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 578);
+            this.Controls.Add(this.tbBuscar);
+            this.Controls.Add(this.lbBusqueda);
+            this.Controls.Add(this.btDer);
+            this.Controls.Add(this.btIzq);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.lbTitulo);
+            this.Name = "FormPrincipal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTitulo;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btIzq;
+        private System.Windows.Forms.Button btDer;
+        private System.Windows.Forms.Label lbBusqueda;
+        private System.Windows.Forms.TextBox tbBuscar;
+    }
+}
+
---
+++ BusquedaSelectivaMontseGarrido/FormPrincipal.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusquedaSelectivaMontseGarrido
+{
+    public partial class FormPrincipal : Form
+    {
+        string cadenaConexion = "server=localhost; database=Montse; user id=sa; password=oretania";
+        ArrayList celdas = new ArrayList();
+
+        public FormPrincipal()
+        {
+            InitializeComponent();
+
+            //Cambiamos el color de fondo, el color y el tipo de la letra al formulario
+            BackColor = Color.FromArgb(184, 240, 237);
+            ForeColor = Color.FromArgb(222, 120, 125);
+            Font = new Font("Comic Sans MS", 11, FontStyle.Bold);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conectar();
+
+            Estilos estilo = new Estilos(this.dgv);
+
+            //Introducimos los diferetentes estilos
+            estilo.definirFilas("#7adcd5", "#b8575e", "#50b4ad", "----------");
+            estilo.definirCabecera("#7adcd5", "#50b4ad");
+            estilo.definirFilarAlternas("#de787d", "#7adcd5", "**********");
+
+            //Ajustamos el tamño de todas las columnas
+            foreach (DataGridViewColumn dgvColumna in this.dgv.Columns)
+            {
+                dgvColumna.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            }
+        }
+
+        private void conectar()
+        {
+            string sentencia = "SELECT * FROM formulario";
+
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                conexion.Open();
+                SqlCommand comando = new SqlCommand(sentencia, conexion);
+                SqlDataReader dataReader = comando.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dataReader);
+                dgv.DataSource = dt;
+                comando.Dispose();
+                conexion.Close();
+
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("No se puede realizar la conexión.");
+            }
+        }
+
+        private void tbBuscar_TextChanged(object sender, EventArgs e)
+        {
+            celdas.Clear();
+            buscarCoincidencias();
+            
+            if (celdas.Count > 0)
+            {
+                dgv.CurrentCell = (DataGridViewCell) celdas[0];
+            } else
+            {
+                dgv.CurrentCell = dgv[0, 0];
+            }
+        }
+
+        private void buscarCoincidencias()
+        {
+            for (int i = 0; i < dgv.Rows.Count - 1; i++)
+            {
+                foreach (DataGridViewCell celda in dgv.Rows[i].Cells)
+                {
+                    if (tbBuscar.Text != "" && celda.Value.ToString().Contains(tbBuscar.Text))
+                    {
+                        celdas.Add(celda);
+                        celda.Style.BackColor = Color.White;
+                    } else
+                    {
+                        celda.Style.BackColor = Color.FromArgb(184, 240, 237);
+                    }
+                }
+            }
+        }
+
+        private void btIzq_Click(object sender, EventArgs e)
+        {
+            if (celdas.Count > 0)
+            {
+                int indice = celdas.IndexOf(dgv.CurrentCell);
+
+                if (indice > 0)
+                {
+                    dgv.CurrentCell = (DataGridViewCell)celdas[indice - 1];
+                }
+            }
+        }
+
+        private void btDer_Click(object sender, EventArgs e)
+        {
+            if (celdas.Count > 0)
+            {
+                int indice = celdas.IndexOf(dgv.CurrentCell);
+
+                if (indice < celdas.Count - 1)
+                {
+                    dgv.CurrentCell = (DataGridViewCell)celdas[indice + 1];
+                }
+            }
+        }
+    }
+}
